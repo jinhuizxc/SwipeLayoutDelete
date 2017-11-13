fa80a53f7dd613edf466603054d2e34d5d3dc81e
JinHui <hui.jin@boatchina.net>
2017-11-13 10:03:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/swipelayoutdelete/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.swipelayoutdelete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 鉴于自己的github上没有写过侧滑删除，项目中也正好需要所以写个侧滑删除，也算是学习。
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
